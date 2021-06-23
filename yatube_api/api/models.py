4c25a2c9926c2416310fd186fb0f1750daeb60d7
vsemikin <vsemikin@yandex.ru>
2021-06-23 14:34:14 +0300

    Ver 0.1

--- yatube_api/api/models.py
+++ yatube_api/api/models.py
@@ -71,7 +71,7 @@ class Follow(models.Model):
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                fields=["user", "author"],
+                fields=["user", "following"],
                 name="unique_pair"
             )
         ]
